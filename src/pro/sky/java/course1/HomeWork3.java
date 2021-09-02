860f4a427ddca3d12164e73c9fb6e1208a9483bb
Vladimir <probavov@gmail.com>
2021-09-02 20:41:17 +0300

    Full

--- src/pro/sky/java/course1/HomeWork3.java
+++ src/pro/sky/java/course1/HomeWork3.java
@@ -1,4 +1,10 @@
 package pro.sky.java.course1;
 
 public class HomeWork3 {
+    public static void main(String[] args) {
+        for (int i = 1900; i < 2500 ; i = i + 4) {
+            System.out.println(i);
+
+        }
+    }
 }
